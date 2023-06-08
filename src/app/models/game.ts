8f20609070948b2ca10e285d28374f199fd60a23
Micro_ice <andrei.drozdov1@mail.ru>
2023-06-09 00:14:38 +0300

    extend data/improve game-card

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -2,20 +2,27 @@ import MeetingNote from "./meeting-note";
 
 type gameJSON = {
   name: string ,
+  alternativeName: string,
   averageTime: number,
+  playerCount: {min: number, max: number};
   shortDescription: string,
   fullDescription: string,
   rules: string,
-  img: string
+  img: string,
+  hot: boolean
 }
 
-export default class Game {
+export default class Game implements gameJSON{
 
   private static id = 0;
 
   public id: number;
 
   public name: string;
+
+  public alternativeName: string;
+
+  public playerCount: {min: number, max: number};
 
   public averageTime: number;
 
@@ -27,6 +34,8 @@ export default class Game {
 
   public img: string;
 
+  public hot: boolean;
+
   notes: MeetingNote[] = [];
 
   constructor(g: gameJSON) {
@@ -35,11 +44,14 @@ export default class Game {
     Game.id++;
 
     this.name = g.name;
+    this.alternativeName = g.alternativeName;
     this.averageTime = g.averageTime;
+    this.playerCount = g.playerCount;
     this.shortDescription = g.shortDescription;
     this.fullDescription = g.fullDescription;
     this.rules = g.rules;
     this.img = g.img;
+    this.hot = g.hot;
   }
 
   public static fromJsonArray(arr: gameJSON[]): Game[] {
@@ -52,5 +64,7 @@ export default class Game {
 
     return games;
   }
+
+
 }
 
